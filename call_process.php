f5030dbb3f653ece2ba9f83ae500a6d20deb537d
Jy-Liu <b03502040@ntu.edu.tw>
2017-01-25 10:21:29 +0800

    transplant 'call'

---
+++ call_process.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('includes/custom/php/general.php');
+
+global $db;
+
+// receive a json array
+$req = $_REQUEST['request'];
+// get current time with year:month:date hour:minute:second format
+date_default_timezone_set("Asia/Taipei");
+$curTime = date("Y-m-d H:i:s");
+// get shop id for the user
+$shop_id = $_SESSION['shop_id'];
+if ($shop_id == '') $shop_id = -2;  // -2: Error session
+echo $shop_id;
+
+// iterate req
+foreach ($req as $item) {
+	// insert data into ingredient table
+	$sql = "INSERT INTO `ingredient` (`shop_id`, `ingredient`, `num`,
+									  `unit`, `call_time`, `handled`)
+			VALUES ('".$shop_id."', '".$item['ingredient']."', '".$item['num']."',
+			 		'".$item['unit']."', '".$curTime."', 0)";
+
+	$result = $db->query($sql);
+}
+
+/*****
+ 						ingredient table
+shop_id     ingredient     num     unit     call_time     handled
+
+*****/
+
+?>
---
+++ templates/includes/js/call.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  $('#submit').on('click', function() {
+  	// configure a request array
+  	var req = [];
+    $('input[type=number]').each(function() {
+    	// find the item
+			var item = $(this);
+			// ingredient name
+			var name = $(this).parent().parent().parent().parent().parent().find("td:first").text();
+			// non zero and non empty
+    	if (item.val() != "" && item.val() != "0") {
+					// configure a new request
+				var _req = {};
+				_req = {
+					"ingredient": name,
+					"num": item.val(),
+					"unit": item.parent().text().replace(/\s+/g, '') // remove space
+				}
+				req.push(_req);
+			}
+			// clear input
+			item.val('');
+    });
+
+    if (req.length == 0) {
+    	alert('請至少選取一項補料，且數量大於０')
+    	return;
+    }
+
+  	console.log(JSON.stringify(req));
+
+  	// ----- send calls to call_process.php -----
+	 	$.ajax( {
+			url: "call_process.php",
+			method: "POST",
+			dataType: "text",
+			data: {request: req}
+		} )
+		.done(function(msg){
+			console.log('Submit a call!');
+			alert('已經叫料');
+			// setTimeout(function(){ alert('已經叫料'); }, 1000);
+		})
+		.fail(function(jqXHR, textStatus, errorThrown){
+			console.log(textStatus, errorThrown);
+			alert('叫料失敗！')
+		})
+		.always(function(msg){
+      //alert(msg)
+		});
+
+  });
+});
