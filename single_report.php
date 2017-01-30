09be4740e8146070689816814d2da6a3ce08f2a2
isaac60103 <isaac60103@gmail.com>
2017-01-30 11:47:52 +0800

    modify link path on html

---
+++ single_report.php
@@ -0,0 +1,21 @@
+<?php
+
+// 此檔案為新頁面用範本檔
+
+require_once ("includes/custom/php/general_define.php");
+
+$_PAGE_TITLE = 'Sample'; // 請輸入頁面抬頭
+$header_type = HEADER_TYPE_SBADMIN2; // 請參考includes/custom/php/general_define.php
+$nav_type = NAV_TYPE_SBADMIN2;       // 請參考includes/custom/php/general_define.php
+$footer_type = FOOTER_TYPE_SBADMIN2; // 請參考includes/custom/php/general_define.php
+require_once('includes/custom/php/header.php');
+require_once('includes/custom/php/navigation.php');
+
+$template = $twig->loadTemplate('single_report.html');
+
+$_HTML .= $template->render(array(
+));
+
+require_once('includes/custom/php/footer.php');
+
+?>
