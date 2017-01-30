e5e437baa7de8dc3d7b1d59cb718d9351d0c73b1
Cliff Wu <z800201002005@gmail.com>
2017-01-30 15:20:17 +0800

    Apply template on permission mange page

---
+++ permission_mange.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once ("includes/custom/php/general_define.php");
+
+$_PAGE_TITLE = '權限管理 - 樂台茶餐飲管理系統';
+$header_type = HEADER_TYPE_SBADMIN2;
+$nav_type = NAV_TYPE_SBADMIN2;
+$footer_type = FOOTER_TYPE_SBADMIN2;
+require_once('includes/custom/php/header.php');
+require_once('includes/custom/php/navigation.php');
+
+$template = $twig->loadTemplate('permission_mange.html');
+
+$_HTML .= $template->render(array(
+));
+
+require_once('includes/custom/php/footer.php');
+
+?>
