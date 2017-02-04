bd7e056cb2561e1eb6e9bd6639ed0915f1460776
isaac60103 <isaac60103@gmail.com>
2017-02-04 12:39:58 +0800

    finish dashboard

--- dashboard_request.php
+++ dashboard_request.php
@@ -1,13 +1,36 @@
 <?php
-require_once('includes/general.php');
+require_once('includes/custom/php/general.php');
 
 header("Content-Type:text/html; charset=utf-8");
 
 $panel_request = $_REQUEST["dash_request"];
-echo 'test';
-echo $panel_request;
 
 
+//echo json_encode("hello");
+
+
+$res_array = array();
+
+$sql = "SELECT COUNT(shop_id) FROM `shop`" ;
+$num_store = $db->query_select_one($sql);
+
+
+$sql = "SELECT COUNT(call_id) FROM `ingredient` WHERE `handled` = 0" ;
+$num_call_material = $db->query_select_one($sql);
+
+
+$sql = "SELECT COUNT(shop_id) FROM `shop`" ;
+$num_activity_store = $db->query_select_one($sql);
+
+$sql = "SELECT COUNT(u_id) FROM `user`" ;
+$num_user = $db->query_select_one($sql);
+
+$res_array['num_store'] = $num_store[0]; 
+$res_array['num_call_material'] = $num_call_material[0]; 
+$res_array['num_activity_store'] = $num_activity_store[0]; 
+$res_array['num_user'] = $num_user[0]; 
+
+echo json_encode($res_array,JSON_UNESCAPED_UNICODE);
 
 ?>
 
--- templates/dashboard.js
+++ templates/dashboard.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    request_panel_info();
     setInterval(request_panel_info, 1000);
 });
 
@@ -9,23 +10,23 @@ function request_panel_info(){
     $.ajax( {
         url:"dashboard_request.php",
         method: "POST",
-        dataType:"text",
+        dataType:"json",
         data: {"dash_request":'1'}
     } )
     .done(function(msg){
+        $("#num_store").text(msg['num_store']);
+        $("#num_call_material").text(msg['num_call_material']);
+        $("#num_activity_store").text(msg['num_activity_store']);
+        $("#num_user").text(msg['num_user']);
 
     })
     .fail(function(){
-        alert("dash panel request fail");
+        console.log("dash panel request fail");
     })
-    .always(function(){
+    .always(function(msg){
+        //console.log('dash panel request success');
 
     });
-
-
-
-
-
     
    return; 
 }
