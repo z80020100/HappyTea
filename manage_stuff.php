42a136d130b1e116e58ff725610b23c8af987c16
Jy-Liu <b03502040@ntu.edu.tw>
2017-02-07 14:47:08 +0800

    transfer add_raw -> manage_stuff

--- manage_stuff.php
+++ manage_stuff.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once ("includes/custom/php/general_define.php");
+require_once('includes/custom/php/general.php');
 
-$_PAGE_TITLE = '編輯物料 - 樂台茶餐飲管理系統';
+$_PAGE_TITLE = '編輯補料 - 樂台茶餐飲管理系統';
 // remember check $uniform_theme in includes/custom/php/general.php
 $header_type = HEADER_TYPE_SBADMIN2;
 $nav_type = NAV_TYPE_SBADMIN2;
@@ -10,9 +10,26 @@ $footer_type = FOOTER_TYPE_SBADMIN2;
 require_once('includes/custom/php/header.php');
 require_once('includes/custom/php/navigation.php');
 
+global $db;
+
+// redirect if it's not top_admin || top_staff
+// if (!(is_topboss() || is_headquarters_staff())) {
+//   header("location:login.php?shop_id=" . $_shopID);
+//   die('');
+// }
+$shop_id = $_SESSION['shop_id'];
+if ($shop_id == '') $shop_id = -2;  // -2: Error session
+
+
+// read data from raw_material table
+$sql = "SELECT `raw_id`, `raw_name`, `unit` FROM `raw_material`";
+
+$result = $db->query($sql);
+
 $template = $twig->loadTemplate('manage_stuff.html');
 
 $_HTML .= $template->render(array(
+  'data' => $result,
 ));
 
 require_once('includes/custom/php/footer.php');
---
+++ templates/includes/js/manage_stuff.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    $('#submit').on('click', function() {
+        // configure a request array
+        var req = [];
+        $('#form tr').each(function() {
+            // get raw name
+            var raw_name = $(this).children().find('input[name=name]');
+            // get unit
+            var unit = $(this).children().find('input[name=unit]');
+            // non undefined && non empty
+            if (raw_name.val() != undefined && raw_name.val() != "" &&
+                unit.val() != undefined && unit.val() != "") {
+                // configure a new request
+                var _req = {};
+                _req = {
+                    "raw_name": raw_name.val().replace(/\s+/g, ''),
+                    "unit": unit.val().replace(/\s+/g, '') // remove space
+                }
+                req.push(_req);
+                // clear input
+                raw_name.val('');
+                unit.val('');
+            }
+        });
+        if (req.length == 0) {
+            alert('請至少新增一項新的補料，且名稱單位皆須填寫')
+            return;
+        }
+
+        console.log(JSON.stringify(req));
+
+        // ----- send calls to call_process.php -----
+        $.ajax({
+                url: "manage_stuff_process.php",
+                method: "POST",
+                dataType: "text",
+                data: {
+                    request: req,
+                    op: 'add_raw'
+                }
+            })
+            .done(function(msg) {
+                console.log('Submit a call!');
+                alert('已經新增');
+                // setTimeout(function(){ alert('已經叫料'); }, 1000);
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+                alert('叫料失敗！')
+            })
+            .always(function() {
+                window.location.reload();
+            });
+
+    });
+
+    $("button[id^='delete_']").on('click', function() {
+        console.log("raw_id = " + $(this).val());
+        var raw_id = $(this).val();
+
+        $.ajax({
+                url: "manage_stuff_process.php",
+                method: "POST",
+                dataType: "text",
+                data: {
+                    'raw_id': raw_id,
+                    op: 'del_raw'
+                }
+            })
+            .done(function(msg) {
+                console.log('Submit a call!');
+                alert('刪除成功');
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+                alert('刪除失敗！')
+            })
+            .always(function() {
+                window.location.reload();
+            });
+    });
+});
