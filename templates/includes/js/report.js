e2214e9035cdb78c99d498374f7f44aabdc43226
Jy-Liu <b03502040@ntu.edu.tw>
2017-02-14 18:42:51 +0800

    make pie chart -> donut with hidden label

--- templates/includes/js/report.js
+++ templates/includes/js/report.js
@@ -1,4 +1,63 @@
-//Flot Pie Chart
+$(document).ready(function() {
+    var start = '2016-01-01 00:00:00';
+    var end = '2018-01-01 23:59:59';
+    var shop = 1;
+
+    var req = queryLog(start, end, shop);
+    console.log(JSON.stringify(req));
+
+    $.ajax({
+        url: "report_process.php",
+        method: "POST",
+        dataType: "json",
+        data: {request: req}
+    })
+    .done(function(msg) {
+        console.log('query log success!');
+        // console.log(JSON.stringify(msg));
+        drawPieChart();
+        drawLineChart();
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // need to change ajax return type into 'text' to see error msg
+        console.log(textStatus, errorThrown);
+        alert('cannot query report!');
+    })
+    .always(function() {
+
+    });
+
+
+});
+
+
+/* === Global variable === */
+var threshold = 0.05    // for drawing pie chart hovering threshold
+/* ===      End        === */
+
+// Get all the Log data for a shop query
+function queryLog(start, end, shop) {
+    var req = {
+        op: "query",
+        start: start,
+        end: end,
+        shop: shop
+    }
+    return req;
+}
+
+function labelFormatter(label, series) {
+    return "<div style='font-size:8pt; text-align:center; padding:2px; color:black;'>" + label + "<br/>" + Math.round(series.percent * 10)/10 + "%</div>";
+}
+
+function makeTooltipContent(label, xval, yval, flotItem) {
+    if (flotItem.series.percent < threshold*100) {
+        return "%p.1%, %s"; // show percentages, rounding to 1 decimal places
+    }
+    return false;
+}
+
+// Flot Pie Chart
 function drawPieChart() {
 
     var data = [{
@@ -18,25 +77,37 @@ function drawPieChart() {
     var plotObj = $.plot($("#flot-pie-chart"), data, {
         series: {
             pie: {
-                show: true
+                show: true,
+                radius: 1,
+                innerRadius: 0.5,
+                label: {
+                    show: true,
+                    radius: 3/4,
+                    formatter: labelFormatter,
+                    threshold: threshold
+                }
             }
+        },
+        legend: {
+            show: false
         },
         grid: {
             hoverable: true
         },
-        tooltip: true,
-        tooltipOpts: {
-            content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
+        tooltip: {
+            show: true,
+            content: makeTooltipContent,
             shifts: {
                 x: 20,
                 y: 0
             },
-            defaultTheme: false
+            defaultTheme: false,
+            // onHover: func(flotItem, $tooltipEl) -> action
         }
     });
 }
 
-//Flot Line Chart
+// Flot Line Chart
 function drawLineChart() {
 
     var offset = 0;
@@ -86,44 +157,3 @@ function drawLineChart() {
             options);
     }
 }
-
-// Get all the Log data for a shop query
-function queryLog(start, end, shop) {
-    var req = {
-        op: "query",
-        start: start,
-        end: end,
-        shop: shop
-    }
-    return req;
-}
-
-$(document).ready(function() {
-    var start = '2016-01-01 00:00:00';
-    var end = '2018-01-01 23:59:59';
-    var shop = 1;
-
-    var req = queryLog(start, end, shop);
-    console.log(JSON.stringify(req));
-
-    $.ajax({
-        url: "report_process.php",
-        method: "POST",
-        dataType: "json",
-        data: {request: req}
-    })
-    .done(function(msg) {
-        console.log('query log success!');
-        console.log(JSON.stringify(msg));
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        // need to change ajax return type into 'text' to see error msg
-        console.log(textStatus, errorThrown);
-        alert('cannot query report!');
-    })
-    .always(function() {
-
-    });
-
-
-});
