0b4dba456875d3ae9e2cd52baa014ec23bddb51a
Cliff Wu <z800201002005@gmail.com>
2017-02-17 01:15:13 +0800

    Add dynamic permission control for staff, but DB is not ready

--- includes/custom/php/permission_function.php
+++ includes/custom/php/permission_function.php
@@ -2,6 +2,21 @@
 
 require_once("permission_define.php");
 
+$permission_list = (1 << 2); // only for testing, should be query from DB
+
+console(LEVEL_DBG, "\$permission_list = $permission_list", __FUNCTION__, __LINE__);
+/* set the file_name element and staff permission */
+while(current($PERMISSION_TABLE)){
+    $file_name = key($PERMISSION_TABLE);
+    $PERMISSION_TABLE[$file_name]['file_name'] = $file_name;
+    if((1 << $PERMISSION_TABLE[$file_name]['bits']) & $permission_list){
+        console(LEVEL_DBG, "Staff can access $file_name", __FUNCTION__, __LINE__);
+        $PERMISSION_TABLE[$file_name]['permission'] |= AUSTAFF;
+    }
+    next($PERMISSION_TABLE);
+}
+/*****************************/
+
 function checkPermission($file_name){
     global $PERMISSION_TABLE;
     console(LEVEL_DBG, $file_name, __FUNCTION__, __LINE__);
