58f73e0a90844a89e4022da9f77b81a1afc89a54
Jy-Liu <b03502040@ntu.edu.tw>
2017-02-16 15:27:49 +0800

    Finish Line Chart

--- templates/includes/js/report.js
+++ templates/includes/js/report.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var shop = 1;
 
     var req = queryLog(start, end, shop);
-    console.log(JSON.stringify(req));
+    // console.log(JSON.stringify(req));
 
     $.ajax({
         url: "report_process.php",
@@ -51,7 +51,8 @@ function handleLog(log) {
     // console.log(JSON.stringify(log));
     var pie_chart_data = getPieChartData(log);
     drawPieChart(pie_chart_data);
-    drawLineChart();
+    var line_chart_data = getLineChartData(log);
+    drawLineChart(line_chart_data);
 }
 
 function getPieChartData(log) {
@@ -78,25 +79,43 @@ function getLineChartData(log) {
 
     data = [];
     for (var i = 0; i < log.length; i++) {
-        var series = log[i].s_text;
+        var time = log[i].time.split(' ')[0];
         var total_price = log[i].price * log[i].quantity;
 
-        var result = $.grep(data, function(e){ return e['label'] == series; });
+        var result = $.grep(data, function(e){ return e[0] == time; });
         if (result.length == 0) {
-            var obj = {label: series, data: total_price}
+            var obj = [time, total_price];
             data.push(obj);
         }
         else {
             // For the result.length is always 1, we can did result[0]
-            result[0]['data'] = parseInt(result[0]['data']) + total_price;
+            result[0][1] = parseInt(result[0][1]) + total_price;
         }
     }
 
-    function gd(year, month, day) {
-       return new Date(year, month, day).getTime();
+    data.sort();
+    // console.log(data);
+
+    for (var i = 0; i < data.length; i++) {
+        var year = data[i][0].split('-')[0];
+        var month = data[i][0].split('-')[1];
+        var day = data[i][0].split('-')[2];
+        data[i][0] = DateGetTime(year, month, day);
     }
 
+    return data;
+}
 
+function DateGetTime(year, month, day) {
+    // .getTime() will return the millisec since 1970/01/01
+    // BUT!!! the month parameter should -1 and then can give as argument
+    // e.g. Jan -> 00, Feb -> 01 and so on
+    // http://stackoverflow.com/questions/19738795/calculate-date-time-from-date-gettime
+    return new Date(year, month-1, day).getTime();
+}
+
+function TimeSetDate(digit) {
+    return new Date(digit);
 }
 
 function labelFormatter(label, series) {
@@ -109,6 +128,8 @@ function makeTooltipContent(label, xval, yval, flotItem) {
     }
     return false;
 }
+
+
 
 // Flot Pie Chart
 function drawPieChart(data) {
@@ -149,21 +170,16 @@ function drawPieChart(data) {
 }
 
 // Flot Line Chart
-function drawLineChart() {
+function drawLineChart(data) {
 
-function gd(year, month, day) {
-       return new Date(year, month, day).getTime();
-    }
-function sd(digit) {
-   return new Date(digit);
-}
-var data1 = [[1262304000000, 7], [1264982400000, 7], [1267401600000, 10], [1270080000000, 13], [1272672000000, 16], [1275350400000, 20], [1277942400000, 22], [1280620800000, 21], [1283299200000, 19], [1285891200000, 15], [1288569600000, 10], [1291161600000, 8]];
-var dataset = [
-    { label: "Gold Price", data: data1}
-];
-    alert(sd(data1[0][0]));
-    alert(gd(2016, 00, 01));
-    alert(sd(1451577600000));
+    /* Reference:
+    http://www.pikemere.co.uk/blog/flot-how-to-create-line-graphs
+    *****/
+
+    var dataset = [
+        { label: "Sales Price", data: data}
+    ];
+
     var options = {
         series: {
             lines: {
@@ -186,25 +202,45 @@ var dataset = [
             axisLabel: "Month",
             axisLabelUseCanvas: true,
             axisLabelFontSizePixels: 12,
-            axisLabelFontFamily: 'Verdana, Arial',
-            axisLabelPadding: 10
+            axisLabelFontFamily: 'Verdana, Arial, Helvetica, sans-serif',
+            axisLabelPadding: 3
         },
         yaxis: {
-            axisLabel: "Gold Price(USD)",
+            axisLabel: "Sales Price",
             axisLabelUseCanvas: true,
             axisLabelFontSizePixels: 12,
-            axisLabelFontFamily: 'Verdana, Arial',
+            axisLabelFontFamily: 'Verdana, Arial, Helvetica, sans-serif',
             axisLabelPadding: 3,
         },
-        tooltip: true,
-        tooltipOpts: {
-            content: "'%s' of %x.1 is %y.4",
+        tooltip: {
+            show: true,
+            content: makeTooltipContent,
             shifts: {
                 x: -60,
-                y: 25
+                y: -30
             }
         }
     };
+
+    function makeTooltipContent(label, xval, yval, flotItem) {
+        var Mon2Num = {
+            'Jan': 1,
+            'Feb': 2,
+            'Mar': 3,
+            'Apr': 4,
+            'May': 5,
+            'Jun': 6,
+            'Jul': 7,
+            'Aug': 8,
+            'Sep': 9,
+            'Oct': 10,
+            'Nov': 11,
+            'Dec': 12
+        }
+        var d_m_d_y = TimeSetDate(flotItem.datapoint[0]).toString().split(' ').slice(0, 4);
+        var datestring = d_m_d_y[3] + '/' + Mon2Num[d_m_d_y[1]] + '/' + d_m_d_y[2];
+        return datestring + ': $' + flotItem.datapoint[1];
+    }
 
     var plotObj = $.plot($("#flot-line-chart"), dataset, options);
 }
