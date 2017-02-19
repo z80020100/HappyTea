85f89bd5fc7b0238ac7a99859f95e66e49caedfa
Cliff Wu <z800201002005@gmail.com>
2017-02-19 08:53:27 +0800

    Modify checkPermission() for request page and update $PERMISSION_TABLE

--- includes/custom/php/permission_define.php
+++ includes/custom/php/permission_define.php
@@ -4,29 +4,80 @@ define("ALL_PEOPLE", AUADMIN|AUSTAFF|AUCUSTOMER|AUGUEST);
 define("ALL_STAFF",  AUADMIN|AUSTAFF);
 
 $PERMISSION_TABLE = array();
-
-$PERMISSION_TABLE['dashboard.php']   = array(
+/*******************************************************************************/
+$PERMISSION_TABLE['account_edit.php'] = array(
     'bits' => count($PERMISSION_TABLE),
-    'file_name' => '', // will be set on while loop in permission_function.php
-    'desc' => '系統總覽',
-    'visible' => true,
-    'permission' => ALL_STAFF
-);
-
-$PERMISSION_TABLE['staff_login.php']   = array(
-    'bits' => count($PERMISSION_TABLE),
-    'file_name' => '', // will be set on while loop in permission_function.php
-    'desc' => '員工登入',
-    'visible' => true,
-    'permission' => ALL_PEOPLE
-);
-
-$PERMISSION_TABLE['account_edit.php']   = array(
-    'bits' => count($PERMISSION_TABLE),
-    'file_name' => '', // will be set on while loop in permission_function.php
+    'file_name' => '', // it will be set on while loop in permission_function.php
     'desc' => '帳號編輯',
-    'visible' => true,
-    'permission' => AUADMIN
+    'link' => '', // if this page is for request, enter the related php which has the same permission
+    'permission' => AUADMIN // if "links" is not null string, this value will be ignored
 );
-
+$PERMISSION_TABLE['edit_account_request.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => 'Request page for 帳號編輯',
+    'link' => 'account_edit.php', // if this page is for request, enter the related php which has the same permission
+    'permission' => '' // if "links" is not null string, this value will be ignored
+);
+/*******************************************************************************/
+$PERMISSION_TABLE['account_management.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => '帳號新增',
+    'link' => '', // if this page is for request, enter the related php which has the same permission
+    'permission' => AUADMIN // if "links" is not null string, this value will be ignored
+);
+/*******************************************************************************/
+$PERMISSION_TABLE['call_stuff.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => '上游叫料',
+    'link' => '', // if this page is for request, enter the related php which has the same permission
+    'permission' => AUADMIN // if "links" is not null string, this value will be ignored
+);
+$PERMISSION_TABLE['call_stuff_process.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => 'Request page for 上游叫料',
+    'link' => 'call_stuff.php', // if this page is for request, enter the related php which has the same permission
+    'permission' => '' // if "links" is not null string, this value will be ignored
+);
+/*******************************************************************************/
+$PERMISSION_TABLE['called_stuff.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => '叫料通知',
+    'link' => '', // if this page is for request, enter the related php which has the same permission
+    'permission' => AUADMIN // if "links" is not null string, this value will be ignored
+);
+$PERMISSION_TABLE['called_stuff_process.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => 'Request page for 叫料通知',
+    'link' => 'called_stuff.php', // if this page is for request, enter the related php which has the same permission
+    'permission' => '' // if "links" is not null string, this value will be ignored
+);
+/*******************************************************************************/
+$PERMISSION_TABLE['dashboard.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => '系統總覽',
+    'link' => '', // if this page is for request, enter the related php which has the same permission
+    'permission' => AUADMIN // if "links" is not null string, this value will be ignored
+);
+$PERMISSION_TABLE['dashboard_request.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => 'Request page for 系統總覽',
+    'link' => 'dashboard.php', // if this page is for request, enter the related php which has the same permission
+    'permission' => '' // if "links" is not null string, this value will be ignored
+);
+/*******************************************************************************/
+$PERMISSION_TABLE['edit_menu.php'] = array(
+    'bits' => count($PERMISSION_TABLE),
+    'file_name' => '', // it will be set on while loop in permission_function.php
+    'desc' => '編輯菜單',
+    'link' => '', // if this page is for request, enter the related php which has the same permission
+    'permission' => AUADMIN // if "links" is not null string, this value will be ignored
+);
 ?>
--- includes/custom/php/permission_function.php
+++ includes/custom/php/permission_function.php
@@ -2,16 +2,24 @@
 
 require_once("permission_define.php");
 
-$permission_list = (1 << 2); // only for testing, should be query from DB
+$permission_list = 0b11111111111111111111111111111111; // only for testing, should be query from DB
 
 console(LEVEL_DBG, "\$permission_list = $permission_list", __FUNCTION__, __LINE__);
 /* set the file_name element and staff permission */
 while(current($PERMISSION_TABLE)){
     $file_name = key($PERMISSION_TABLE);
     $PERMISSION_TABLE[$file_name]['file_name'] = $file_name;
-    if((1 << $PERMISSION_TABLE[$file_name]['bits']) & $permission_list){
-        console(LEVEL_DBG, "Staff can access $file_name", __FUNCTION__, __LINE__);
-        $PERMISSION_TABLE[$file_name]['permission'] |= AUSTAFF;
+    if($PERMISSION_TABLE[$file_name]['link'] == ''){
+        if((1 << $PERMISSION_TABLE[$file_name]['bits']) & $permission_list){
+            console(LEVEL_DBG, "Staff can access $file_name", __FUNCTION__, __LINE__);
+            $PERMISSION_TABLE[$file_name]['permission'] |= AUSTAFF;
+        }
+    }
+    else{
+        console(LEVEL_DBG, "$file_name has the same permission as ".$PERMISSION_TABLE[$file_name]['link'], __FUNCTION__, __LINE__);
+        if((1 << $PERMISSION_TABLE[$PERMISSION_TABLE[$file_name]['link']]['bits']) & $permission_list){
+            $PERMISSION_TABLE[$file_name]['permission'] = $PERMISSION_TABLE[$PERMISSION_TABLE[$file_name]['link']]['permission'];
+        }
     }
     next($PERMISSION_TABLE);
 }
@@ -21,23 +29,44 @@ function checkPermission($file_name){
     global $PERMISSION_TABLE;
     console(LEVEL_DBG, $file_name, __FUNCTION__, __LINE__);
     if(!isset($PERMISSION_TABLE[$file_name])){
-        console(LEVEL_ERR, "此頁面未設定權限！", __FUNCTION__, __LINE__);
+        die('此頁面未設定權限！');
+    }
+    else if(!isset($PERMISSION_TABLE[$file_name]['bits'])){
+        die("!isset(\$PERMISSION_TABLE[\$file_name]['bits'])");
+    }
+    else if(!isset($PERMISSION_TABLE[$file_name]['file_name'])){
+        die("!isset(\$PERMISSION_TABLE[\$file_name]['file_name'])");
+    }
+    else if(!isset($PERMISSION_TABLE[$file_name]['desc'])){
+        die("!isset(\$PERMISSION_TABLE[\$file_name]['desc'])");
+    }
+    else if(!isset($PERMISSION_TABLE[$file_name]['link'])){
+        die("!isset(\$PERMISSION_TABLE[\$file_name]['link'])");
+    }
+    else if(!isset($PERMISSION_TABLE[$file_name]['permission'])){
+        die("!isset(\$PERMISSION_TABLE[\$file_name]['permission'])");
     }
     else{
         console(LEVEL_DBG, "Bits = ".$PERMISSION_TABLE[$file_name]['bits'], __FUNCTION__, __LINE__);
         console(LEVEL_DBG, "File Name = ".$PERMISSION_TABLE[$file_name]['file_name'], __FUNCTION__, __LINE__);
         console(LEVEL_DBG, "Description = ".$PERMISSION_TABLE[$file_name]['desc'], __FUNCTION__, __LINE__);
-        console(LEVEL_DBG, "Visible = ".$PERMISSION_TABLE[$file_name]['visible'], __FUNCTION__, __LINE__);
+        console(LEVEL_DBG, "Link = ".$PERMISSION_TABLE[$file_name]['link'], __FUNCTION__, __LINE__);
         console(LEVEL_DBG, "Permission = ".$PERMISSION_TABLE[$file_name]['permission'], __FUNCTION__, __LINE__);
         
         if(checkAuth($PERMISSION_TABLE[$file_name]['permission'])){
             console(LEVEL_DBG, "允許存取", __FUNCTION__, __LINE__);
+            //echo '允許存取';
+        }
+        else if($PERMISSION_TABLE[$file_name]['link'] == ''){
+            console(LEVEL_DBG, "禁止存取", __FUNCTION__, __LINE__);
+            //echo '禁止存取';
+            header("refresh:0;url=denied.php");
         }
         else{
-            console(LEVEL_DBG, "禁止存取", __FUNCTION__, __LINE__);
-            header("refresh:0;url=denied.php");
+            console(LEVEL_DBG, "禁止存取request page", __FUNCTION__, __LINE__);
+            //echo '禁止存取request page';
             exit;
-        }     
+        }
     }
 }
 ?>
